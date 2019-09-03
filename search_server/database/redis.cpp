8d90431413a5dc8e8b8654982e87c909f836b537
walekkk <root@localhost.localdomain>
2019-09-03 16:14:43 +0800

    add database use redis

--- search_server/database/redis.cpp
+++ search_server/database/redis.cpp
@@ -1,9 +1,58 @@
 #include <iostream>
 #include "../common/common.h"
 #include "./redis.h"
-int main()
-{
-    std::cout << "Hello world" << std::endl;
-    return 0;
+
+BEGIN_NAMESPACE(search_server, database);
+
+Redis::Redis(): mRedis(NULL), mAddress("") {}
+
+Redis::~Redis() {
+    if (mRedis) {
+        redisFree(mRedis);
+        LOG(INFO, "redis free success");
+    }
 }
 
+bool Redis::Init() {
+    mPort = 8081;
+    *mIp = "127.0.0.0";
+    mRedis = redisConnect(mIp, mPort);
+    if (!mRedis) {
+        LOG(ERROR, "redis init error");
+        return false;
+    }
+    return true;
+}
+
+bool Redis::Push(std::string& key, std::string& value) {
+    if (!mRedis) {
+        LOG(ERROR, "redis not init now");
+        return false;
+    } 
+    redisReply* reply = (redisReply*)redisCommand(mRedis, "SET %s %s",
+            key.c_str(), value.c_str());
+    if (!reply) {
+        LOG(ERROR, "redis insert error");
+        return false;
+    } else if (reply->str != "OK") {
+        LOG(ERROR, "redis insert error because :");
+        cout << reply->str;
+        return false;
+    }
+    return true;
+}
+
+std::string Redis::Get(std::string& key) {
+    if (!mRedis) {
+        LOG(ERROR, "redis not init now");
+        return "";
+    }
+    redisReply* reply = (redisReply*)redisCommand(mRedis, "GET %s",
+            key.c_str());
+    if (!reply) {
+        LOG(ERROR, "dont have this word");
+        return "";
+    }
+    return reply->str;
+}
+END_NAMESPACE;
--- search_server/database/redis.h
+++ search_server/database/redis.h
@@ -1,12 +1,41 @@
-#pragma once
+#ifndef REDIS_H
+#define REDIS_H
 
-class redis
-{
+#include "../common/log.h"
+#include <hiredis/redis.h>
+
+BEGIN_NAMESPACE(search_server, database);
+
+class Redis {
 public:
-    redis() {}
-    ~redis() {}
+    Redis();
+
+    ~Redis();
+
+    void Init();
+
+    bool Push(std::string& key, std::string& value);
+
+    std::string Get(std::string& key);
 
 private:
-
+    redisContext*   mRedis;
+    int             mPort;
+    char*           mIp;
 };
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+END_NAMESPACE;
+#endif
